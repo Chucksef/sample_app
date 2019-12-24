020214926848d0875b1d29399118876bbd579487
Chucksef <Chucksef@gmail.com>
2019-12-24 16:53:31 -0700

    added a flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 
 		if @user.save
+			flash[:success] = "Added #{@user.name} to the Sample App!"
 			redirect_to @user
 		else
 			render :new
